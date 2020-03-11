6eab689f8425bad5d1ec29e6d2381186648a9cf7
Kendy Liên <kendy.lien@beyondedge.co>
2020-03-11 16:59:25 +0700

    add admin theme

--- Modules/mod_content/tmpl/content.php
+++ Modules/mod_content/tmpl/content.php
@@ -130,11 +130,11 @@
 </content>
 <?php
   function url(){
-  return sprintf(
-    "%s://%s",
-    isset($_SERVER['HTTPS']) && $_SERVER['HTTPS'] != 'off' ? 'https' : 'http',
-    $_SERVER['SERVER_NAME']
-    // ,$_SERVER['REQUEST_URI']
-  );
-}
+    return sprintf(
+      "%s://%s",
+      isset($_SERVER['HTTPS']) && $_SERVER['HTTPS'] != 'off' ? 'https' : 'http',
+      $_SERVER['SERVER_NAME']
+      // ,$_SERVER['REQUEST_URI']
+    );
+  }
  ?>
---
+++ admin/modules/mod_slider/tmpl/slider.php
@@ -0,0 +1,1 @@
+<div>Chán gê á</div>

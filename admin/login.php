2eb081c41b942032b6d133ba3c15e12d311231dc
Kendy Liên <kendy.lien@beyondedge.co>
2020-03-15 12:19:55 +0700

    uploads

--- admin/login.php
+++ admin/login.php
@@ -1,157 +1,73 @@
-<!-- <?php if (isset($_POST['login-admin'])) :?>
-	<?php if ($_POST['email-address']=='kendy@gmail.com' && $_POST['password']=='123'): ?>
-  		<?php $_SESSION['login'] = 'kendy';?><meta http-equiv="refresh" content="0;url=<?=url();?>" /><?php ?>
-  	<?php else: ?>
-  		<meta http-equiv="refresh" content="0;url=<?=url();?>" />
-	<?php endif;?>
-<?php else: ?>
-<?php endif; ?>
-<?php
-  function url(){
-    return sprintf(
-      "%s://%s%s",
-      isset($_SERVER['HTTPS']) && $_SERVER['HTTPS'] != 'off' ? 'https' : 'http',
-      $_SERVER['SERVER_NAME'],$_SERVER['REQUEST_URI']
-    );
-  }
- ?>
+<!-- RUSSIAN FILE INPUT -->
+<!-- load the JS files in the right order -->
+<!-- <script src="assets/js/fileinput.js"></script>
+<script src="assets/js/locales/ru.js"></script>
+ 
+<label class="control-label">Выберите Файл</label>
+<div class="file-loading">
+    <input id="input-ru" name="inputru[]" type="file" multiple>
+</div>
+<script>
+$("#input-ru").fileinput({
+    language: "ru",
+    uploadUrl: "assets/img/upload",
+    allowedFileExtensions: ["jpg", "png", "gif"]
+});
+</script> -->
 
 <!DOCTYPE html>
-<html lang="en">
-  <head>
-    <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
-    <meta name="description" content="A fully featured admin theme which can be used to build CRM, CMS, etc.">
+<html>
+<body>
 
-    <!-- Libs CSS -->
-    <link rel="stylesheet" href="./assets/fonts/feather/feather.min.css">
-    <link rel="stylesheet" href="./assets/libs/flatpickr/dist/flatpickr.min.css">
-    <link rel="stylesheet" href="./assets/libs/quill/dist/quill.core.css">
-    <link rel="stylesheet" href="./assets/libs/select2/dist/css/select2.min.css">
-    <link rel="stylesheet" href="./assets/libs/highlight.js/styles/vs2015.css">
+<form action="login.php" method="post" enctype="multipart/form-data">
+    Select image to upload:
+    <input type="file" name="fileToUpload" id="fileToUpload">
+    <input type="submit" value="Upload Image" name="submit">
+</form>
 
-    <!-- Map -->
-    <link href='https://api.mapbox.com/mapbox-gl-js/v0.53.0/mapbox-gl.css' rel='stylesheet' />
-
-    <!-- Theme CSS -->
-      
-    <link rel="stylesheet" href="./assets/css/theme.min.css" id="stylesheetLight">
-
-    <link rel="stylesheet" href="./assets/css/theme-dark.min.css" id="stylesheetDark">
-
-    <style>body { display: none; }</style>
-    
-
-    <title>Dashkit</title>
-  </head>
-  <body class="d-flex align-items-center bg-auth border-top border-top-2 border-primary">
-
-    <!-- CONTENT
-    ================================================== -->
-    <div class="container">
-      <div class="row justify-content-center">
-        <div class="col-12 col-md-5 col-xl-4 my-5">
-          
-          <!-- Heading -->
-          <h1 class="display-4 text-center mb-3">
-            Sign in
-          </h1>
-          
-          <!-- Subheading -->
-          <p class="text-muted text-center mb-5">
-            Free access to our dashboard.
-          </p>
-          
-          <!-- Form -->
-          <form method="post">
-
-            <!-- Email address -->
-            <div class="form-group">
-
-              <!-- Label -->
-              <label>Email Address</label>
-
-              <!-- Input -->
-              <input type="email" class="form-control" placeholder="name@address.com" name="email-address">
-
-            </div>
-
-            <!-- Password -->
-            <div class="form-group">
-
-              <div class="row">
-                <div class="col">
-                      
-                  <!-- Label -->
-                  <label>Password</label>
-
-                </div>
-                <div class="col-auto">
-                  
-                  <!-- Help text -->
-                  <a href="password-reset.html" class="form-text small text-muted">
-                    Forgot password?
-                  </a>
-
-                </div>
-              </div> <!-- / .row -->
-
-              <!-- Input group -->
-              <div class="input-group input-group-merge">
-
-                <!-- Input -->
-                <input type="password" class="form-control form-control-appended" placeholder="Enter your password" name="password">
-
-                <!-- Icon -->
-                <div class="input-group-append">
-                  <span class="input-group-text">
-                    <i class="fe fe-eye"></i>
-                  </span>
-                </div>
-
-              </div>
-            </div>
-
-            <!-- Submit -->
-            <button type="submit" class="btn btn-lg btn-block btn-primary mb-3" name="login-admin" id="login-admin">
-              Sign in
-            </button>
-
-            <!-- Link -->
-            <div class="text-center">
-              <small class="text-muted text-center">
-                Don't have an account yet? <a href="sign-up.html">Sign up</a>.
-              </small>
-            </div>
-            
-          </form>
-
-        </div>
-      </div> <!-- / .row -->
-    </div> <!-- / .container -->
-
-    <!-- JAVASCRIPT
-    ================================================== -->
-    <!-- Libs JS -->
-    <script src="./assets/libs/jquery/dist/jquery.min.js"></script>
-    <script src="./assets/libs/bootstrap/dist/js/bootstrap.bundle.min.js"></script>
-    <script src="./assets/libs/@shopify/draggable/lib/es5/draggable.bundle.legacy.js"></script>
-    <script src="./assets/libs/autosize/dist/autosize.min.js"></script>
-    <script src="./assets/libs/chart.js/dist/Chart.min.js"></script>
-    <script src="./assets/libs/dropzone/dist/min/dropzone.min.js"></script>
-    <script src="./assets/libs/flatpickr/dist/flatpickr.min.js"></script>
-    <script src="./assets/libs/highlightjs/highlight.pack.min.js"></script>
-    <script src="./assets/libs/jquery-mask-plugin/dist/jquery.mask.min.js"></script>
-    <script src="./assets/libs/list.js/dist/list.min.js"></script>
-    <script src="./assets/libs/quill/dist/quill.min.js"></script>
-    <script src="./assets/libs/select2/dist/js/select2.min.js"></script>
-    <script src="./assets/libs/chart.js/Chart.extension.min.js"></script>
-
-    <!-- Map -->
-    <script src='https://api.mapbox.com/mapbox-gl-js/v0.53.0/mapbox-gl.js'></script>
-
-    <!-- Theme JS -->
-    <script src="./assets/js/theme.min.js"></script>
-
-  </body>
-</html> -->
+</body>
+</html>
+<?php
+$target_dir = "assets/img/uploads/";
+$target_file = $target_dir . basename($_FILES["fileToUpload"]["name"]);
+$uploadOk = 1;
+$imageFileType = strtolower(pathinfo($target_file,PATHINFO_EXTENSION));
+// Check if image file is a actual image or fake image
+if(isset($_POST["submit"])) {
+    $check = getimagesize($_FILES["fileToUpload"]["tmp_name"]);
+    if($check !== false) {
+        echo "File is an image - " . $check["mime"] . ".";
+        $uploadOk = 1;
+    } else {
+        echo "File is not an image.";
+        $uploadOk = 0;
+    }
+}
+// Check if file already exists
+if (file_exists($target_file)) {
+    echo "Sorry, file already exists.";
+    $uploadOk = 0;
+}
+// Check file size
+if ($_FILES["fileToUpload"]["size"] > 500000) {
+    echo "Sorry, your file is too large.";
+    $uploadOk = 0;
+}
+// Allow certain file formats
+if($imageFileType != "jpg" && $imageFileType != "png" && $imageFileType != "jpeg"
+&& $imageFileType != "gif" ) {
+    echo "Sorry, only JPG, JPEG, PNG & GIF files are allowed.";
+    $uploadOk = 0;
+}
+// Check if $uploadOk is set to 0 by an error
+if ($uploadOk == 0) {
+    echo "Sorry, your file was not uploaded.";
+// if everything is ok, try to upload file
+} else {
+    if (move_uploaded_file($_FILES["fileToUpload"]["tmp_name"], $target_file)) {
+        echo "The file ". basename( $_FILES["fileToUpload"]["name"]). " has been uploaded.";
+    } else {
+        echo "Sorry, there was an error uploading your file.";
+    }
+}
+?>
